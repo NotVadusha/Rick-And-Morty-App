8c49314c4b2324dfcffac180acc9a0f55028a42b
NotVadusha <vadimkbondarchuk@gmail.com>
2023-07-09 19:52:59 +0300

    fix: replaced functions with const, replace .then with async/await feat: add getCharacter function

--- src/services/rmService.js
+++ src/services/rmService.js
@@ -2,14 +2,24 @@ import axios from "axios";
 
 const apiUrl = 'https://rickandmortyapi.com/api/character'
 
-export function getCharacters(page) {
-    let characters = [];
-    axios.get(apiUrl + `/?page=${page}`).then(pageResult => {
-        characters = pageResult.data.results;
-    }).catch((e) => {
-        throw new Error(e);
-    }).finally(() => {
-            return characters;
-        }
-    )
+const getData = (requestUrl) =>{
+    return new Promise((resolve, reject) => {
+        resolve(axios.get(apiUrl + requestUrl));
+    })
+}
+
+export const getCharactersPage = async (page) => {
+    try {
+        return await getData(`?page=${page}`)
+    } catch (e) {
+        return e;
+    }
+}
+
+export const getCharacter = async (id) => {
+    try {
+        return await getData(`/${id}`)
+    } catch (e) {
+        return e;
+    }
 }
