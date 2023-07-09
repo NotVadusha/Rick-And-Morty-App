5202436a31c8fe923decf5ebd5b4a544a88612ce
NotVadusha <vadimkbondarchuk@gmail.com>
2023-07-09 22:41:48 +0300

    ref: some refactoring

--- src/services/RMService.js
+++ src/services/RMService.js
@@ -1,10 +1,8 @@
-import HttpRmService from "./HTTPService";
 import HttpService from "./HTTPService";
 
 export default class RMService {
     constructor() {
         this.api = new HttpService('https://rickandmortyapi.com/api/character');
-
     }
 
     getCharactersPage = async (page) => {
