32ec4aad0760e61736afb4e8b5870aa54a70d9d1
NotVadusha <vadimkbondarchuk@gmail.com>
2023-07-12 13:11:48 +0300

    feat: add characters list

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -2,7 +2,16 @@ import CharacterCard from "./characterCard";
 
 function CharactersList(pageInfo) {
     return (
-        <></>
+
+        <div className={"container"}>
+            <ul className={"flex flex-wrap"}>
+                {pageInfo.data.map(character => {
+                    console.log(character)
+                    return CharacterCard(character);
+                })}
+            </ul>
+        </div>
+
     );
 }
 
