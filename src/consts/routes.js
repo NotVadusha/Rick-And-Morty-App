5d1d04ec1735f04ebc797cbd4c05e6d855bd4c0a
NotVadusha <vadimkbondarchuk@gmail.com>
2023-07-14 11:10:53 +0300

    fix: split router and add useRoutes()

--- src/consts/routes.js
+++ src/consts/routes.js
@@ -1,20 +1,20 @@
-import {createBrowserRouter} from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import CharacterPage from "../pages/CharacterPage";
 import CharactersService from "../services/CharactersService";
 
 const service = new CharactersService();
 
-export const router = createBrowserRouter([
-    {
+export const routes =
+    [{
         path: "/",
         element: <MainPage data={(await service.getCharactersPage(1)).data.results}/>,
     },
-    {
-        path: "character/:characterId",
-        loader: async (charId) => {
-            return await service.getCharacter(charId.params.characterId);
-        },
-        element: <CharacterPage/>
-    }
-]);
+        {
+            path: "character/:characterId",
+            loader: async (charId) => {
+                return await service.getCharacter(charId.params.characterId);
+            },
+            element: <CharacterPage/>
+        }
+    ];
+
--- src/index.js
+++ src/index.js
@@ -1,35 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MainPage from "./pages/MainPage";
-import CharactersService from "./services/CharactersService";
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
-import CharactersList from "./components/CharactersList";
-import CharacterPage from "./pages/CharacterPage";
-
-const service = new CharactersService();
-
-const router = createBrowserRouter(
-        [
-            {
-                path: "/",
-                element: <MainPage data={(await service.getCharactersPage(1)).data.results}/>,
-            },
-            {
-                path: "character/:characterId",
-                loader: async (charId)=>{
-                    return await service.getCharacter(charId.params.characterId);
-                },
-                element: <CharacterPage/>
-            }
-        ]
-    )
-;
-
-const testRick = JSON.parse(localStorage.getItem('charactersInfo'));
+import App from "./App";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {routes} from "./consts/routes";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
+        <RouterProvider router={createBrowserRouter(routes)}>
+            <App/>
+        </RouterProvider>
     </React.StrictMode>
 );
