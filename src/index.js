d747c8bc5e42cbc0e132399330877dbfa3a51b99
NotVadusha <vadimkbondarchuk@gmail.com>
2023-07-15 19:10:45 +0300

    ref: delete unused import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {RouterProvider} from "react-router-dom";
 import {routes} from "./consts/routes";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
