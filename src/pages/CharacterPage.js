6e5554962d3804b35a99477239d8a45e46e41189
NotVadusha <vadimkbondarchuk@gmail.com>
2023-07-15 19:11:59 +0300

    fix: replace object.keys with object.entries, replace GO BACK Link with button

--- src/pages/CharacterPage.js
+++ src/pages/CharacterPage.js
@@ -1,22 +1,22 @@
-
 import {BiArrowBack} from "react-icons/bi";
-import {Link, useLoaderData} from "react-router-dom";
+import {useLoaderData, useNavigate} from "react-router-dom";
 
 function CharacterPage() {
     const characterInfo = useLoaderData();
     const additionalInfo = {
-        Gender: characterInfo.data.gender,
-        Status: characterInfo.data.status,
-        Specie: characterInfo.data.species,
-        Origin: characterInfo.data.origin.name,
-        Type: characterInfo.data.type,
-    };
+            Gender: characterInfo.data.gender,
+            Status: characterInfo.data.status,
+            Specie: characterInfo.data.species,
+            Origin: characterInfo.data.origin.name,
+            Type: characterInfo.data.type,
+        },
+        prevPage = useNavigate();
 
     return (
         <div className={"flex flex-col items-center mb-32 font-roboto"}>
-            <Link to={"/"} className={"block text-lg font-bold font-karla my-6 mx-20 place-self-start"} href={'#'} >
+            <button onClick={()=>{prevPage(-1)}} className={"block text-lg font-bold font-karla my-6 mx-20 place-self-start"}>
                 <BiArrowBack className={"inline"}/> GO BACK
-            </Link>
+            </button>
 
             <img className={"my-4 rounded-full border-4 border-slate-100 "} src={characterInfo.data.image}
                  alt={characterInfo.data.name}></img>
@@ -24,11 +24,11 @@ function CharacterPage() {
 
             <h2 className={"my-12 text-xl text-neutral-400\t"}>Information</h2>
             <ul className={'w-1/6'}>
-                {Object.keys(additionalInfo).map((key) => {
+                {Object.entries(additionalInfo).map((line) => {
                     return (
                         <li className={"my-2"}>
-                            <h3 className={"text-base font-bold"}>{key}</h3>
-                            <p className={"text-sm text-slate-500 mb-6"}>{additionalInfo[key]}</p>
+                            <h3 className={"text-base font-bold"}>{line[0]}</h3>
+                            <p className={"text-sm text-slate-500 mb-6"}>{line[1].length>0?line[1]:"no info"}</p>
                         </li>
                     )
                 })}
