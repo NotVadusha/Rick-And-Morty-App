576a4e4e31673b4ad56b7512c3b799bf1eb5df2b
NotVadusha <vadimkbondarchuk@gmail.com>
2023-07-17 13:38:50 +0300

    feat: add isError state

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+
+function NotFound() {
+    return (
+        <div className={"container mx-auto mt-16"}>
+            <h2 className={"mx-auto text-center font-roboto text-5xl font-semibold"}>Not found</h2>
+            <p className={"mx-auto text-center font-roboto text-3xl mt-8"}>There is no such characters in Rick&Morty :(</p>
+        </div>
+    )
+}
+
+export default NotFound;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,6 +3,7 @@ import Search from "../components/Search";
 import {useEffect, useState} from "react";
 import {useQuery} from "@tanstack/react-query";
 import {service} from "../services/CharactersService";
+import NotFound from "../components/NotFound";
 
 function MainPage() {
     const [filter, _setFilter] = useState(sessionStorage.getItem("filter") || "");
@@ -11,7 +12,7 @@ function MainPage() {
         ["characters"],
         () => service.filterCharacterName(filter),
         {
-            select: (data) => data?.data?.results
+            select: (data) => data.data.results
         }
     );
 
@@ -23,6 +24,24 @@ function MainPage() {
         _setFilter(value);
         sessionStorage.setItem("filter", value);
     };
+
+    if (isError){
+        return (
+            <>
+                <img
+                    className={"w-152 mx-auto my-3"}
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rick_and_Morty.svg/1920px-Rick_and_Morty.svg.png?20220319060844"
+                    alt="rick&morty logo"
+                />
+                <Search filter={filter} setFilter={setFilter}/>
+                <NotFound />
+            </>
+        )
+    }
+
+    if (isLoading){
+
+    }
 
     return (
         <>
