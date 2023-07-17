59dfa97dd524c84470777536d0a00ac7eba4f157
NotVadusha <vadimkbondarchuk@gmail.com>
2023-07-17 17:12:21 +0300

    feat: add ListSkeleton

---
+++ src/components/ListSkeleton.js
@@ -0,0 +1,21 @@
+function ListSkeleton({cards}) {
+    return (
+        <div>
+            <ul className={"flex flex-wrap flex-none justify-center gap-8"}>
+                {Array(cards).fill(0).map((item) =>
+                    <li>
+                        <div className={"w-64 p-4 rounded-md shadow-md shadow-gray-500/50"}>
+                            <div className={'rounded-md w-56 h-44 mx-auto bg-gray-300'}/>
+                            <div className={'font-roboto my-3'}>
+                                <div className={'w-56 rounded-md h-6 bg-gray-300 mb-2'}/>
+                                <div className={'w-48 rounded-md h-6 bg-gray-300'}/>
+                            </div>
+                        </div>
+                    </li>
+                )}
+            </ul>
+        </div>
+    )
+}
+
+export default ListSkeleton;
