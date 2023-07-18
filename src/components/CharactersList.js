6508012fe5e5883061bb913fcd648395d109d4b7
NotVadusha <vadimkbondarchuk@gmail.com>
2023-07-18 21:45:58 +0300

    fix: parseInt for page, sort names in list

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -1,6 +1,9 @@
 import CharacterCard from "./CharacterCard";
 
 function CharactersList({charactersArray}) {
+    charactersArray.sort((a, b)=>{
+        return a.name > b.name ? 1: -1;
+    });
     return (
         <div className={"container mx-auto py-8"}>
             <ul className={"flex flex-wrap flex-none justify-center gap-8"}>
