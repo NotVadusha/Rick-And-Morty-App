0e57fc547f3b68a5d2d2c31a897621e50cb90b9c
NotVadusha <vadimkbondarchuk@gmail.com>
2023-07-18 22:32:29 +0300

    feat: add sort of characters via their names

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -1,13 +1,13 @@
 import CharacterCard from "./CharacterCard";
 
 function CharactersList({charactersArray}) {
-    charactersArray.sort((a, b)=>{
+    const sortedArray = [...charactersArray].sort((a, b)=>{
         return a.name > b.name ? 1: -1;
     });
     return (
         <div className={"container mx-auto py-8"}>
             <ul className={"flex flex-wrap flex-none justify-center gap-8"}>
-                {charactersArray.map(character => <CharacterCard key={character.id} character={character}/>)}
+                {sortedArray.map(character => <CharacterCard key={character.id} character={character}/>)}
             </ul>
         </div>
     );
